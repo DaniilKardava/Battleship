Fixed 2 bugs in previous change. Made grid energy computation part of update grid. Removed slow polymorphism in update buckets. Code is hairy now but working.

CVersion/energy_analysis.py
@@ -7,7 +7,7 @@ with open("energies.txt") as f:
             values.append(int(chunk))
 energies = np.array(values, dtype=np.int8)
 
-iterations = 3000
+iterations = 1000
 energies = energies.reshape(-1, iterations)
 avg_energies = np.mean(energies, axis = 1)
 energy_variance = np.var(energies, axis = 1) / iterations